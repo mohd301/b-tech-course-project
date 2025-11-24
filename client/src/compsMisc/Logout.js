d44cec414682b1241eab272c183f58d03f6c49fc
Omar Nezar <frcollege2@gmail.com>
2025-11-24 18:03:39 +0400

    Logout component for all users

---
+++ client/src/compsMisc/Logout.js
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../slices/SliceUser";
+import { logoutPriv } from "../slices/SlicePriv";
+import { getUserType } from "../functions/getUserType";
+
+export default function Logout() {
+    const navigate = useNavigate();
+    const logoutDispatch=useDispatch()
+
+    const handleLogout = () => {
+        const type=getUserType()
+        localStorage.removeItem("authToken");
+        if(type==="User"){
+            logoutDispatch(logoutUser());
+        } else if(type==="Admin" || type==="Regulator"){
+            logoutDispatch(logoutPriv())
+        }
+        navigate("/", { replace: true });
+    };
+
+    return (
+        <button className="primaryButton" onClick={handleLogout}>
+            Logout
+        </button>
+    );
+}
