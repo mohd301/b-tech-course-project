1db9e4dc532ce440da096935256e48a4b298f957
Omar Nezar <frcollege2@gmail.com>
2025-11-25 18:34:52 +0400

    Added fallback when unable to determine user type from token

--- client/src/functions/determineRoute.js
+++ client/src/functions/determineRoute.js
@@ -5,8 +5,9 @@ export function determineRoute(type) {
             case "Admin": route = "/homeAdmin"; break;
             case "Regulator": route = "/homeReg"; break;
             case "User": route = "/home"; break;
+            default: route = "/"; break;
         }
-        return route
+        return route;
     } catch (err) {
         console.error("error determining Route", err);
         return null;
