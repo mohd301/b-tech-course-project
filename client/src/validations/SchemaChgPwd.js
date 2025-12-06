d78e064f6ebc81a73d76b2e64eabbf273acb5116
Omar Nezar <frcollege2@gmail.com>
2025-12-06 17:03:53 +0400

    added check that new password is not the same as old

--- client/src/validations/SchemaChgPwd.js
+++ client/src/validations/SchemaChgPwd.js
@@ -2,7 +2,14 @@ import * as yup from 'yup';
 const SchemaChgPwd = yup.object().shape({
     Email: yup.string().required('Email Required'),
     oldPassword: yup.string().required('Password Required'),
-    newPassword: yup.string().required('New Password Required').min(8, 'Too Short').max(18, 'Too Long').matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).+$/, 'Must contain at least one special character, number and letter'), /* regex validating pwd using lookahead statements (?=.*) */
+    newPassword: yup.string().required('New Password Required').min(8, 'Too Short').max(18, 'Too Long')
+        .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).+$/, 'Must contain at least one special character, number and letter') /* regex validating pwd using lookahead statements (?=.*) */
+        .test('not===old','New password cannot be the same as old password',
+            function (value) {
+                const { oldPassword } = this.parent;
+                return value !== oldPassword;
+            }
+        ),
     confpwd: yup.string().required('Password Confirmation Required').oneOf([yup.ref('newPassword')], 'Password must Match')
 })
 export default SchemaChgPwd;
