589140833fd750b7968258a1767737715b952242
Omar Nezar <frcollege2@gmail.com>
2026-02-17 17:51:31 +0400

    Colors.js ow exports 2 theme objects instead of one

--- client/src/styles/colors.js
+++ client/src/styles/colors.js
@@ -1,7 +1,21 @@
-export const colors = {
+export const lightTheme = {
   primaryColor: '#66BB6A', 
   secondaryColor: 'rgb(236, 68, 65)', 
-  tertiaryColor: 'rgb(116, 116, 116)', 
+  tertiaryColor: '#747474', 
   primaryBackground: '#dfe4e6ff', 
-  textColor: '#ECEFF1' 
+  textColor: '#ECEFF1',
+  textError: '#f33c3c', 
+  textColorAlt: '#000000',
+  altBackground: '#ffffff',
+};
+
+export const darkTheme = {
+  primaryColor: '#29802d', 
+  secondaryColor: 'rgb(163, 53, 52)', 
+  tertiaryColor: 'rgb(104, 104, 104)', 
+  primaryBackground: 'rgb(24, 24, 24)', 
+  textColor: '#ECEFF1',
+  textError: '#d43e3e',
+  textColorAlt: '#d1d1d1',
+  altBackground: '#000000',  
 };
