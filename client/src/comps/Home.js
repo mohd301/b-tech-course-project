5782359e498a18df1a88cb82ef20297492fbe5fc
Omar Nezar <frcollege2@gmail.com>
2026-02-22 19:58:37 +0400

    Moved Logout to Navbar

--- client/src/comps/Home.js
+++ client/src/comps/Home.js
@@ -1,4 +1,3 @@
-import Logout from "../compsMisc/Logout";
 import { useTheme } from "../compsMisc/ThemeContext.js";
 
 export default function Home() {
@@ -6,7 +5,6 @@ export default function Home() {
     return (
         <div className="d-flex flex-column justify-content-center align-items-center" style={{ minHeight: "80vh" }}>
             <h1 style={{color: theme.textColorAlt}}> Welcome User </h1>
-            <Logout/>
         </div>
     )
 }
--- client/src/compsMisc/Navbar.js
+++ client/src/compsMisc/Navbar.js
@@ -4,24 +4,27 @@ import { FaUser } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 import { useTheme } from "./ThemeContext.js";
+import Logout from "./Logout.js";
 
 export default function Navbar() {
     const { theme } = useTheme();
 
     const user = useSelector(state => state.user.user)
+    const priv = useSelector(state => state.priv.priv)
     return (
-        <Container fluid style={{ background: theme.secondaryColor, minHeight: '5vh' }} className="p-4 d-flex position-relative align-items-center justify-content-center">
-            {user && (
+        user && (
+            <Container fluid style={{ background: theme.secondaryColor, minHeight: '5vh' }} className="p-4 d-flex position-relative align-items-center justify-content-center">
                 <div className="profileMenu" style={{ position: 'absolute', left: '1rem', fontSize: '1.5rem' }}>
                     <div className="iconButton d-flex align-items-center justify-content-center">
                         <FaUser />
                     </div>
 
                     <div className="dropdownMenu">
-                        <Link style={{color: theme.textColor}} to="changePwd">Change Password</Link>
+                        <Link style={{ color: theme.textColor }} to="changePwd">Change Password</Link>
+                        <Logout />
                     </div>
                 </div>
-            )}
-        </Container>
+            </Container>
+        )
     )
 }
