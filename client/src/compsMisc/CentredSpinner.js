0d2940ddd49672795d0417069ffd7f58476ffdd1
Omar Nezar <frcollege2@gmail.com>
2026-02-24 15:18:38 +0400

    Spinner can now accept theme colors

--- client/src/compsMisc/CentredSpinner.js
+++ client/src/compsMisc/CentredSpinner.js
@@ -1,9 +1,9 @@
 import { Container, Spinner } from "reactstrap";
 
-export default function CenteredSpinner({ height, color = "light"}) {
+export default function CenteredSpinner({ height, color = "white"}) {
     return (
-        <Container className="d-flex align-items-center justify-content-center" style={{height}}>
-            <Spinner color={color} />
+        <Container className="d-flex align-items-center justify-content-center" style={{height, color}}>
+            <Spinner />
         </Container>
     )
 }
