4ced5dba200b278f35521caf1c26730b7f3bea2e
Omar Nezar <frcollege2@gmail.com>
2026-02-24 15:25:09 +0400

    Improved Navbar rendering logic

--- client/src/comps/Header.js
+++ client/src/comps/Header.js
@@ -2,18 +2,19 @@ import { Container } from "reactstrap";
 import Navbar from "../compsMisc/Navbar.js"
 import { useDispatch, useSelector } from "react-redux"
 import { useTheme } from "../compsMisc/ThemeContext.js";
+import { useEffect } from "react";
 
 export default function Header() {
     const { theme } = useTheme();
+    const token = useSelector((state) => state.auth.token)
 
-    const user = useSelector(state => state.auth.user)
     return (
         <>
             <Container fluid style={{ background: theme.primaryColor, minHeight: '7vh' }} className="p-4 d-flex align-items-center justify-content-center">
                 <h1 style={{ color: "white" }}>Online Subsidy Eligibility System</h1>
             </Container>
 
-            {user && (
+            {token && (
                 <Navbar />
             )}
         </>
