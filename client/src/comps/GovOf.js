71350ccbd86accc73447e530612f91d0726bac86
Mohammed aloufi <mohammed.oufi@targetdigitalventures.com>
2026-02-25 20:05:12 +0400

    working on map

--- client/src/comps/GovOf.js
+++ client/src/comps/GovOf.js
@@ -7,14 +7,14 @@ export default function GovOf() {
     return (
         <>
             <Container fluid>
-                <div style={{ background: theme.primaryBackground, minHeight: "82.1vh" }}>
+                <div style={{ background: theme.secondaryColor, minHeight: "82.1vh" }}>
                     <div className="d-flex justify-content-center align-items-center">
-                        <Card style={{background: theme.tertiaryColor, minHeight: "68vh", width: "50vw", borderRadius: "6vh" }}
+                        <Card style={{background: theme.secondaryColor, minHeight: "68vh", width: "50vw", borderRadius: "6vh" }}
                         className="d-flex justify-content-center mt-4 mb-4">
                             <CardTitle className="p-4">
                             <Label>Map of govermemt offices</Label>
                             </CardTitle>
-                            <CardBody className="m-3" style={{display:"flex",alignItems:"center"}}>
+                            <CardBody className="d-flex justify-content-center mt-4 mb-4" style={{display:"flex",alignItems:"center"}}>
                             <Maps />
                             </CardBody>
                         </Card>
