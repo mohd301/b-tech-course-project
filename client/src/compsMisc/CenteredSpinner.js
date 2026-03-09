d0086a8bfbfb8cb99d142550cd08e327b6295d3a
Omar Nezar <frcollege2@gmail.com>
2026-03-09 16:25:46 +0400

    Now accepts a size parameter and defaults to large

--- client/src/compsMisc/CenteredSpinner.js
+++ client/src/compsMisc/CenteredSpinner.js
@@ -1,9 +1,9 @@
 import { Container, Spinner } from "reactstrap";
 
-export default function CenteredSpinner({ height, color = "white"}) {
+export default function CenteredSpinner({ height, color = "white", size = "lg" }) {
     return (
         <Container className="d-flex align-items-center justify-content-center" style={{height, color}}>
-            <Spinner />
+            <Spinner size={size} />
         </Container>
     )
 }
