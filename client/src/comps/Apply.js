94a82c63e291be267fdde259240b487af0d19bf0
Omar Nezar <frcollege2@gmail.com>
2026-03-16 17:17:18 +0400

    Fixed formatting + incorrect use of thunk

--- client/src/comps/Apply.js
+++ client/src/comps/Apply.js
@@ -1,20 +1,22 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 import { Button, Input, Card, Form, Label, CardBody, CardImg, CardHeader, CardFooter } from "reactstrap"
-import { useTheme } from "../compsMisc/ThemeContext"
-import { useSelector,useDispatch } from "react-redux"
-import { decryptToken } from "../functions/decryptToken"
+import { useSelector, useDispatch } from "react-redux"
 import { FiXCircle } from "react-icons/fi";
 import { FiCheckCircle } from "react-icons/fi";
+
+import { useTheme } from "../compsMisc/ThemeContext"
+import { decryptToken } from "../functions/decryptToken"
+import { userApplyThunk } from "../slices/SliceUser"
 export default function Apply() {
     const token = localStorage.getItem("authToken")
     const user = decryptToken(token)
     console.log(user)
     const { theme } = useTheme()
     const [Data, Setdata] = useState()
-    const [res,Setres]=useState("")
+    const [res, Setres] = useState("")
     const dispatch = useDispatch()
-    const data = useSelector((state)=>state.user.data)
+    const data = useSelector((state) => state.user.data)
     async function getdata(data) {
         // try {
         //     console.log(ID)
@@ -27,10 +29,10 @@ export default function Apply() {
         // } catch (e) {
         //     console.log(e)
         // }
-        try{
-            const res = await dispatch(userApplyThunk).unwrap()
+        try {
+            const res = await dispatch(userApplyThunk(data)).unwrap()
             return res
-        }catch(e){
+        } catch (e) {
             console.log(e)
         }
     }
@@ -39,13 +41,11 @@ export default function Apply() {
     }
     console.log(res.data?.Eligibity)
     console.log(res.data?.reson)
-    const [ID, SetId] = useState()
-    const userdata={
+    const [ID, SetId] = useState("")
+    const userdata = {
         ID,
-        _id:user.id,
-        Email:user.Email,
-
-
+        _id: user.id,
+        Email: user.Email,
     }
 
     const onsubmit = () => {
@@ -64,39 +64,39 @@ export default function Apply() {
                                 </div>
                                 <Label style={{ color: theme.textColorAlt }}>Enter your ID</Label>
                                 <Input type="text" name="ID" style={{ width: "45%" }} value={ID} onChange={handleid} placeholder="ID"></Input>
-                                {res==="!"?(
-                                res.data?.Eligibity === 1 ? (
-                                    <Card style={{ background: theme.tertiaryColor, minHeight: "20vh", width: "15vw", borderRadius: "6vh" }} 
-                                    className="d-flex justify-content-center mt-4 mb-4 logRegCard">
-                                        <CardHeader className="d-flex justify-content-center">
-                                            <FiCheckCircle style={{color:theme.primaryColor}} className="justify-content-center"
-                                         size={"3em"} />
-                                        </CardHeader>
-                                        <CardFooter>
-                                    <p style={{ color: theme.textColorAlt }} className="text-center">Eligible</p>
-                                    </CardFooter>
-                                    </Card>
-                                ) : (
-                                    <Card style={{ background: theme.tertiaryColor, minHeight: "20vh", width: "16vw", borderRadius: "6vh" }}
-                                     className="d-flex justify-content-center mt-4 mb-4 logRegCard">
-                                        <CardHeader className="d-flex justify-content-center"
-                                        ><FiXCircle style={{color:theme.secondaryColor}} className="justify-content-center"
-                                         size={"3em"} /></CardHeader>
-                                         <CardFooter>
-                                    <p style={{ color: theme.textColorAlt }} className="text-center">Not Eligible</p>
-                                    
-                                    <p style={{ color: theme.textColorAlt }} className="text-center">{res.data?.reson} is too high</p>
-                                    </CardFooter>
-                                    </Card>
-                                ))
-                                :(
-                                    <></>
-                                )}
+                                {res === "!" ? (
+                                    res.data?.Eligibity === 1 ? (
+                                        <Card style={{ background: theme.tertiaryColor, minHeight: "20vh", width: "15vw", borderRadius: "6vh" }}
+                                            className="d-flex justify-content-center mt-4 mb-4 logRegCard">
+                                            <CardHeader className="d-flex justify-content-center">
+                                                <FiCheckCircle style={{ color: theme.primaryColor }} className="justify-content-center"
+                                                    size={"3em"} />
+                                            </CardHeader>
+                                            <CardFooter>
+                                                <p style={{ color: theme.textColorAlt }} className="text-center">Eligible</p>
+                                            </CardFooter>
+                                        </Card>
+                                    ) : (
+                                        <Card style={{ background: theme.tertiaryColor, minHeight: "20vh", width: "16vw", borderRadius: "6vh" }}
+                                            className="d-flex justify-content-center mt-4 mb-4 logRegCard">
+                                            <CardHeader className="d-flex justify-content-center"
+                                            ><FiXCircle style={{ color: theme.secondaryColor }} className="justify-content-center"
+                                                size={"3em"} /></CardHeader>
+                                            <CardFooter>
+                                                <p style={{ color: theme.textColorAlt }} className="text-center">Not Eligible</p>
+
+                                                <p style={{ color: theme.textColorAlt }} className="text-center">{res.data?.reson} is too high</p>
+                                            </CardFooter>
+                                        </Card>
+                                    ))
+                                    : (
+                                        <></>
+                                    )}
                                 <div className="d-flex align-items-end justify-content-end mt-5">
-                                    
+
                                     <Button style={{ background: theme.primaryColor }} onClick={onsubmit}>Sumbit</Button>
                                 </div>
-                                
+
 
                             </CardBody>
                         </Card>
