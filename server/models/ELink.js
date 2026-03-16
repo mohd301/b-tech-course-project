3ea1fb73504e9d053e5fe65c5b6824e58e8d7494
Mohammed aloufi <mohammed.oufi@targetdigitalventures.com>
2026-03-16 16:23:19 +0400

    working on new databaselink

---
+++ server/models/ELink.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const ELinkSchema = new mongoose.Schema(
+    {
+        Email: { type: String, required: true },
+        UserID: { type: String, required: true },
+        NationalID: { type: Number, required: true },
+        Fraud: { type: Number, required: true, default: 0 },
+        Eligibility:{type:Number,required:true},
+        Reason:{type:String,required:false}
+    },
+    {
+        versionKey: false
+    }
+)
+
+const ELinkModel = new mongoose.model("eligibility link", ELinkSchema)
+
+export default ELinkModel
